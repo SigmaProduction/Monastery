073541efb3c0a658c41f4b9c7cb825e0cc3b173d
Neo <neo@Neos-MacBook-Pro.local>
2023-06-19 20:08:30 +0700

    feat(admin): add admin lte and login feature

---
+++ app/Http/Controllers/AdminAuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminAuthController extends Controller
+{
+    /**
+     * Show the admin login form.
+     *
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function showLoginForm()
+    {
+        return view('admin.auth.login');
+    }
+
+    /**
+     * Handle the admin login request.
+     *
+     * @param  Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ], [
+            'email.required' => 'Please enter your email address.',
+            'email.email' => 'Please enter a valid email address.',
+            'password.required' => 'Please enter your password.',
+        ]);
+
+        $remember = $request->filled('remember');
+
+        if (Auth::attempt($credentials, $remember)) {
+            // Authentication passed
+            return redirect()->route('admin.index');
+        }
+
+        // Authentication failed
+        return redirect()->back()->withErrors([
+            'email' => 'Invalid credentials',
+        ]);
+    }
+
+    public function logout()
+    {
+        Auth::guard()->logout();
+
+        return redirect()->route('login');
+    }
+}
---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.admin') <!-- Assuming you have a layout file for the admin panel -->
+
+@section('content')
+    <h1>Welcome to the Admin Dashboard</h1>
+@endsection
